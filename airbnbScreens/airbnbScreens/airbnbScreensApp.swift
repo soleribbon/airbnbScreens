d277b6adc9f6fabb8269415fdf1bf19f805a429b
Ravi <ivar202@gmail.com>
2022-11-14 17:31:43 +0100

    First commit

---
+++ airbnbScreens/airbnbScreens/airbnbScreensApp.swift
@@ -0,0 +1,17 @@
+//
+//  airbnbScreensApp.swift
+//  airbnbScreens
+//
+//  Created by Ravi  on 11/14/22.
+//
+
+import SwiftUI
+
+@main
+struct airbnbScreensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
